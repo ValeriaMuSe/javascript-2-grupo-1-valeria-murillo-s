0aea29155ad9643740b74be2be98480dd9406683
Vale <valeriams13@gmail.com>
2023-01-17 17:56:26 -0600

    prueba-diagnostico

---
+++ Prueba-Diagnostico/main.js
@@ -0,0 +1,74 @@
+// Prueba Diagnostico
+
+// I Parte: Teoría
+
+// 1. Respuesta pregunta 1
+
+// String: ("hola mundo"), Number: (13), Boolean (true)
+
+// ====================================================================================================
+
+
+// 2. Respuesta pregunta 2
+
+// Colocar la respuesta acá
+
+// ====================================================================================================
+
+
+// 3. Respuesta pregunta 3
+
+// Lo que hace es a la lista original se le suma lo de la otra lista, entonces sería a 1231 + 3234 y el console.log resulta con 4465
+
+// ====================================================================================================
+
+
+// 4. Respuesta pregunta 4
+
+// Colocar la respuesta acá
+
+// ====================================================================================================
+
+
+// 5. Respuesta pregunta 5
+
+// Colocar la respuesta acá
+
+// ====================================================================================================
+
+
+// 6. Respuesta pregunta 6
+
+// Colocar la respuesta acá
+
+// ====================================================================================================
+
+
+// 7. Respuesta pregunta 7
+
+// Colocar la respuesta acá
+
+// ====================================================================================================
+
+
+// 8. Respuesta pregunta 8
+
+// A lo que recuerdo, recursividad es de realizar una función con if y else y repetir en console.log lo que es el inicio de la función
+
+// ====================================================================================================
+
+
+// II Parte: Ejercicios
+
+// 1. Respuesta pregunta 1
+
+// Colocar el código acá
+
+// ====================================================================================================
+
+
+// 2. Respuesta pregunta 2
+
+// Colocar el código acá
+
+// ====================================================================================================
